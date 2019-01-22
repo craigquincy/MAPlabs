6e99089200055d8c2ab66cb39da78e0205beefae
Nick Griffen <nicholas.s.griffen@gmail.com>
2019-01-21 21:07:41 -0700

    explicit constants docs

--- src/store/answers/constants.js
+++ src/store/answers/constants.js
@@ -1,8 +1,34 @@
 /**
- * @module
+ * @module src/store/answers/constants
+ */
+
+/**
+ * @constant
+ * @type {string}
+ * @default
  */
 export const ANSWERS_LOADING  = "ANSWERS_LOADING"
+/**
+ * @constant
+ * @type {string}
+ * @default
+ */
 export const ANSWERS_LOAD     = "ANSWERS_LOAD"
+/**
+ * @constant
+ * @type {string}
+ * @default
+ */
 export const ANSWERS_ERROR_DB = "ANSWERS_ERROR_DB"
+/**
+ * @constant
+ * @type {string}
+ * @default
+ */
 export const ANSWERS_UPDATE   = "ANSWERS_UPDATE"
+/**
+ * @constant
+ * @type {string}
+ * @default
+ */
 export const ANSWERS_NO_OP    = "ANSWERS_NO_OP"
