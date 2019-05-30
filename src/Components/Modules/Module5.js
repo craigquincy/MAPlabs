a43ada22b7174e7de7e03e478e5d78462f2f4eb4
Katie Hoesley <katiehoesley@gmail.com>
2019-05-30 17:27:45 -0600

    updated module 5

--- src/Components/Modules/Module5.js
+++ src/Components/Modules/Module5.js
@@ -45,12 +45,14 @@ export default class Module5 extends React.Component {
 
 
 strengths_510 = [
-    // <ShortAnswersCT question = { { code: 510, text: `Building your MAPmaker Dashboard` } } />,
-    <ShortAnswersCT question = { { code: 510, text: `Revise and condense your Current Situation Statement` } } />,
-    <ShortAnswersCT question = { { code: 511, text: `Add to your Dashboard your key themes` } } />,
-    <ShortAnswersCT question = { { code: 512, text: `Summarize the context of your purpose-seeking life` } } />,
-    <ShortAnswersCT question = { { code: 513, text: `Review these contextual elements of your Dashboard and write in your journal` } } />
-    //under this have a fetch to get what THEY wrote in each module
+    <ShortAnswersCT question = { { code: 510, text: `Revise and consolidate this statement down into a simple, 
+    to-the-point synopsis of only the most important concepts in a few sentences below. 
+    Here's what you wrote: //FETCH THEIR PREVIOUS RESPONSE` } } />,
+    <ShortAnswersCT question = { { code: 511, text: `Add to your Dashboard your key themes. Review your themes to consider 
+    how accurately they reflect your current self-knowledge and understanding.` } } />,
+    <ShortAnswersCT question = { { code: 512, text: `Summarize the context of your purpose-seeking life. 
+    Add any summarizing thoughts or directives that you feel will be helpful to you in the future as a quick reference, 
+    reminder, or accountability statement about your key themes.` } } />,
 ]
 
 exercise_510 = (
@@ -62,13 +64,25 @@ exercise_510 = (
 
 
 strengths_520 = [
-    <ShortAnswersCT question = { { code: 520, text: `Add to your Dashboard your key insights about meaning` } } />,
-    <ShortAnswersCT question = { { code: 521, text: `Add to your Dashboard your key insights about your desires` } } />,
-    <ShortAnswersCT question = { { code: 522, text: `Add to your Dashboard your key insights about your strengths` } } />,
-    <ShortAnswersCT question = { { code: 523, text: `Summarize who you are as a purpose seeker` } } />,
-    <ShortAnswersCT question = { { code: 524, text: `Review the 'who' elements of your Dashboard and write in your journal about any insights or
-     implications these elements have on your sense of who you are as a pursuer of life purpose.` } } />
-
+    <ShortAnswersCT question = { { code: 520, text: `Add to your Dashboard your key insights about meaning. 
+    Review your responses about what you found most meaningful. 
+    Here's what you wrote: //FETCH THEIR PREVIOUS RESPONSE` } } />,
+    <ShortAnswersCT question = { { code: 521, text: `Choose only the 5 most critical themes above
+    and copy them into the boxes below. You may also revise the statements to better fit with your 
+    current self-knowledge and understanding.` } } />,
+    <ShortAnswersCT question = { { code: 522, text: `Add to your Dashboard your key insights about your desires. 
+    Review your Desires Statement below
+    and revise or edit it to more accurately reflect your current self-knowledge and understanding. If possible, 
+    consolidate your statement down into a simple, to-the-point synopsis of only the most important desires in a 
+    few sentences. Here's what you wrote: //FETCH FOR DESIRES STATEMENT` } } />,
+    <ShortAnswersCT question = { { code: 523, text: `Add to your Dashboard your key insights about your strengths.
+     Review your responses about what you found most meaningful and consider how accurately they reflect your current 
+     self-knowledge and understanding. //fetch and display top 5 strenghts, embodiment
+    themes, impediment themes ` } } />,
+    <ShortAnswersCT question = { { code: 524, text: `Choose only the most critical strengths and themes 
+    from question #4 and copy them into the boxes below. You may also revise the statements to better fit with 
+    your current self-knowledge and understanding. //NEED THREE BOXES THERE THEY CAN
+    ENTER THEIR STRENGHTS (4), EMBODIMENTS (4), IMPEDIMENTS (4)` } } />
 ]
 
 //DONT ACTUALLY WANT THESE AS SHORT ANSWER, WANT THEM AS A DISPLAY OF THAT DATA THAT WE
@@ -84,7 +98,9 @@ exercise_520 = (
 
 
 strengths_530 = [
-    <ShortAnswersCT question = { { code: 530, text: `Add to your Dashboard your key insights about beyond-the-self service` } } />,
+    <ShortAnswersCT question = { { code: 530, text: `Add to your Dashboard your key insights about beyond-the-self service. 
+    Review your beyond-the-self service themes to consider how accurately they reflect your current 
+    self-knowledge and understanding. //FETCH FOR PREVIOUS THEMES WRITTEN` } } />,
     <ShortAnswersCT question = { { code: 531, text: `Summarize the 'what' of your purpose` } } />,
     <ShortAnswersCT question = { { code: 532, text: `Review the 'what' elements of your Dashboard and write in your journal about any
      insights or implications these specific perspectives have on your sense of life purpose, especially
@@ -103,10 +119,15 @@ exercise_530 = (
 
 
 strengths_540 = [
-    <ShortAnswersCT question = { { code: 540, text: `Bring the Dashboard elements together into a purpose statement` } } />,
-    <ShortAnswersCT question = { { code: 541, text: `Review your Purpose Statement and write in your journal about any insights
-     or implications that seeing this statement has on how you want to live your life..` } } />,
-
+    <ShortAnswersCT question = { { code: 540, text: `Bring the Dashboard elements together into a purpose statement. 
+    Review your beyond-the-self service themes to consider how accurately they reflect your current self-knowledge 
+    and understanding. //FETCH THEIR THEMES FROM PREVIOUS MODULES` } } />,
+    <ShortAnswersCT question = { { code: 541, text: `Choose only the 5 most critical themes above 
+    and copy them into the boxes below. You may also revise the statements to better fit with 
+    your current self-knowledge and understanding. You may revise your themes in the boxes below.` } } />,
+    <ShortAnswersCT question = { { code: 541, text: `Summarize the "what" of your purpose. Add any summarizing thoughts 
+    or directives that you feel will be helpful to you in the future as a quick reference, reminder, or 
+    accountability statement about what you care about and want to serve that is beyond yourself.` } } />
 ]
 
 exercise_540 = (
@@ -120,11 +141,15 @@ exercise_540 = (
 
 
 strengths_550 = [
-    <ShortAnswersCT question = { { code: 550, text: `Add to your Dashboard your key commitments about personal growth` } } />,
-    <ShortAnswersCT question = { { code: 551, text: `Add to your Dashboard your key commitments about relationships` } } />,
-    <ShortAnswersCT question = { { code: 552, text: `Add to your Dashboard your key commitments about engagement mastery` } } />,
-    <ShortAnswersCT question = { { code: 553, text: `Rank the commitments on your Dashboard and consolidate` } } />,
-    <ShortAnswersCT question = { { code: 554, text: `Review the Commitments section of your Dashboard and write in your journal` } } />,
+    <ShortAnswersCT question = { { code: 550, text: `Consider all of the elements on your Dashboard. 
+    Then articulate your own personal life purpose statement. Review the items on your Dashboard on the last tab in 
+    this workbook and then fill out the Purpose Statement below.
+    <br><br>
+    My life is most meaningful when I experience or embrace<br>____(Source(s) of meaning)_______.
+    That is why my greatest desire is to<br>_____(Desire)______.
+    by using my<br>_____(Strengths)______.
+    in service of<br>_____(Something or someone beyond the self)______.
+    ` } } />,
 
 ]
 
@@ -140,8 +165,25 @@ exercise_550 = (
 
 
 strengths_560 = [
-    <ShortAnswersCT question = { { code: 560, text: `Review and revise your Break and Build Matrix` } } />,
-    <ShortAnswersCT question = { { code: 561, text: `Revise and consense your Future Desired Situation` } } />,
+    <ShortAnswersCT question = { { code: 560, text: `Add to your Dashboard your key commitments about personal growth.
+    Review your personal growth commitments to consider how accurately they reflect your current self-knowledge and understanding.
+    //WHAT YOU WROTE FROM M1-M4
+    ` } } />,
+    <ShortAnswersCT question = { { code: 561, text: `Choose only the 5 most critical themes in column C and copy them into the boxes below. 
+    You may also revise the statements to better fit with your current self-knowledge and understanding. 
+    You may revise your themes in the boxes below.` } } />,
+    <ShortAnswersCT question = { { code: 562, text: `Add to your Dashboard your key relationships.
+    Review your relationships to consider how accurately they reflect your current self-knowledge and understanding.
+    You may revise your themes in the boxes below.` 
+    } } />,
+    <ShortAnswersCT question = { { code: 563, text: `Add to your Dashboard your key engagement mastery. Review your 
+    engagement mastery to consider how accurately these themes reflect your current self-knowledge and understanding.
+    //ADD IN EVERYTHING THEY WROTE FOR M1-M4 (???)` 
+    } } />,
+    <ShortAnswersCT question = { { code: 564, text: `Choose only the 5 most critical themes in column O (????) and copy 
+    them into the boxes below. You may also revise the statements to better fit with your current 
+    self-knowledge and understanding. You may revise your themes in the boxes below.` 
+    } } />
 ]
 
 exercise_560 = (
@@ -153,7 +195,17 @@ exercise_560 = (
 
 
 strengths_570 = [
-    <ShortAnswersCT question = { { code: 570 } } />,
+    <ShortAnswersCT question = { { code: 570, text: `Review your responses to how
+    you would like to alter how you engage with the following: HOW ARE WE GOING TO FORMAT THIS SECTION??`} } />,
+    <ShortAnswersCT question = { { code: 571, text: `Choose only the 10 most critical changes from columns G and I 
+    and copy them into the boxes below. You may also revise the statements to better fit with your current self-knowledge 
+    and understanding. HOW ARE WE GOING TO FORMAT THIS SECTION??`} } />,
+    <ShortAnswersCT question = { { code: 572, text: `Review and revise your Desired Future Situation statement. Revise 
+    and consolidate this statement down into a simple, to-the-point synopsis of only the most important concepts 
+    in a few sentences. Here's what you wrote: //FETCH FOR THEIR RESPONSE//
+
+    You may revise your statement in the box below.
+    `} } />,
 ]
 
 exercise_570 = (
@@ -219,7 +271,7 @@ exercise_570 = (
         <SectionCT
             moduleNum = { 5 }
             sectionNum = { 570 }
-            sectionTitle = "Conclusion"
+            sectionTitle = "Review your Break and Build Matrix"
             exercise = {this.exercise_570}
         />  
       </ModuleCT>
